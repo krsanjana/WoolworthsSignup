3d2f9d909da769659e11f712b9e7e10213ceb520
krsanjana <64631329+krsanjana@users.noreply.github.com>
2020-11-22 22:34:14 +1100

    QA task - Woolworths login and signup

---
+++ woolworthsLogin.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+context('Window', () => {
+
+  it('Open Woolworths URL', () => {
+   cy.visit('https://www.woolworths.com.au/') 
+  })
+
+  it('click on login button', () => {
+    cy.get('*[class*="coreHeader-loginText"]').first().click() 
+  })
+
+  it('enter username and password', () =>{
+
+    cy.get('input[name="Email"]').type('sanjanakr@mailinator.com')
+    cy.get('input[name="Password"]').type('qwert123')
+    cy.get('button[type="submit"]').type('{enter}')
+
+  })
+
+  it('add watermelon to the cart', () => {
+
+      cy.get('#headerSearch').type('watermelon')
+      cy.get('i[class="iconAct-Search search-button-icon"]').click()
+      cy.wait(2000)
+      cy.get('a[class="shelfProductTile-descriptionLink"]').first().click()
+      cy.wait(2000)
+      cy.get('span[class="cartControls-addCart"]').click()
+      cy.wait(2000)
+      cy.get('a[class="headerCheckout-cartButton headerCheckout-cartButtonHover guestOpenCart"]').click()
+
+  })
+})
